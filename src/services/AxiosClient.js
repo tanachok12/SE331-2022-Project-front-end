30c4872f7000408a4a808414d8b52ea55ea60c58
tanachok12 <tanachok_d@elearning.ac.th>
2022-10-23 19:59:56 +0700

    add prod

--- src/services/AxiosClient.js
+++ src/services/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://44.204.208.235:8999',
+  baseURL: 'http://3.231.31.27:8999',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
